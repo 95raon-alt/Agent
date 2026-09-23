e0c4371f6cbadd697d41c4a40f3b880daabe1340
95raon-alt <95.raon@gmail.com>
2026-09-23 23:15:34 +0900

    09.23 23:15

--- scratch/verify.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('UIUX_허지민/Antigravity/05.수정/hero_test.html', 'utf8');
-console.log('File size:', html.length, 'bytes');
-console.log('Has #hero:', html.includes('id="hero"'));
-console.log('Has WINNER PRIZE:', html.includes('WINNER'));
-console.log('Has High End Wellness:', html.includes('High End Wellness'));
-console.log('Has SCROLL DOWN:', html.includes('SCROLL'));
